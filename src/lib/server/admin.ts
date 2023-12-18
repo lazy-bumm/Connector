691848e0bae964252d5d543b891342be438dbfb3
lazy_bum <rumaankhan48@gmail.com>
2023-12-18 13:27:17 +0530

    D

--- src/lib/server/admin.ts
+++ src/lib/server/admin.ts
@@ -1,22 +1,20 @@
-
-import { getFirestore } from 'firebase/firestore';
-import { FB_CLIENT_EMAIL, FB_PRIVATE_KEY, FB_PROJECT_ID } from '$env/static/private'
-import pkg from 'firebase-admin';
 import { getAuth } from 'firebase-admin/auth';
+import { getFirestore } from 'firebase-admin/firestore';
+import pkg from 'firebase-admin';
 
-// try {
-//   pkg.initializeApp({
-//     credential: pkg.credential.cert({
-//       projectId: FB_PROJECT_ID,
-//       clientEmail: FB_CLIENT_EMAIL,
-//       privateKey: FB_PRIVATE_KEY,
-//     }),
-//   });
-// } catch (err: any) {
-//   if (!/already exists/u.test(err.message)) {
-//     console.error('Firebase Admin Error: ', err.stack)
-//   }
-// }
+try {
+  pkg.initializeApp({
+    credential: pkg.credential.cert({
+      projectId: import.meta.env.FB_PROJECT_ID,
+      clientEmail: import.meta.env.FB_CLIENT_EMAIL,
+      privateKey: import.meta.env.FB_PRIVATE_KEY,
+    }),
+  });
+} catch (err: any) {
+  if (!/already exists/u.test(err.message)) {
+    console.error('Firebase Admin Error: ', err.stack)
+  }
+}
 
 
 export const adminDB = getFirestore();
