846e4237cf761ecf7a2ce27fa75053a114937025
lazy_bum <rumaankhan48@gmail.com>
2023-12-18 12:51:37 +0530

    npm install fixed

--- svelte.config.js
+++ svelte.config.js
@@ -1,33 +1,18 @@
-// import adapter from '@sveltejs/adapter-auto';
-// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-// 	// for more information about preprocessors
-// 	preprocess: vitePreprocess(),
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
 
-// 	kit: {
-// 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-// 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-// 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-// 		adapter: adapter()
-// 	}
-// };
-
-// export default config;
-
-
-import adapter from '@sveltejs/adapter-netlify';
-
-export default {
-  kit: {
-    adapter: adapter(),
-	edge: false,
-
-	// if true, will split your app into multiple functions
-	// instead of creating a single one for the entire app.
-	// if `edge` is true, this option cannot be used
-	split: false
-  }
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
+	}
 };
+
+export default config;
